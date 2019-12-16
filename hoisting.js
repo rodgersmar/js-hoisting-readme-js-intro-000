8fbbdac5901cb177450834d3c40fed3276df5c21
toto <ro.marcus@free.fr>
2019-12-16 18:07:43 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,23 @@
-function callMe() {
+/*function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
   var lyric = "maybe";
+}*/
+
+function callMe() {
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  var lyric = "maybe";
+  alert(lyric);
+  return lyric;
+  //var lyric = "maybe";
 }
+
 
 
 function crazy() {
